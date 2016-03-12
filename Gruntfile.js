ce8652b1aab8b1adc14591e48c21aba0bff0090a
Jason <jason.rammoray@gmail.com>
2016-03-12 20:57:41 +0300

    Ability to use path to custom job

---
+++ Gruntfile.js
@@ -0,0 +1,129 @@
+/*
+ * grunt-ng-html-snapshot
+ * https://github.com/jasonrammoray/grunt-ng-html-snapshot
+ *
+ * Copyright (c) 2016 JasonRammoray
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    clean: {
+      tests: ['tmp']
+    },
+
+    ng_html_snapshot: {
+      options: {
+        // Keep in mind, that you have to have a running web server, which serves content at baseUrl
+
+        baseUrl: 'https://docs.angularjs.org',
+
+        jobPath: 'utils/phantom-job.js',
+
+        output: {
+          dir: './tmp'
+        },
+
+        // Page loading timeout in ms. Default - 100 ms
+
+        pageLoadingTimeout: 100,
+
+        pageDocType: '<!doctype html>',
+
+        pages: [
+          {
+            path: '/guide',
+
+            file: 'guide'
+          },
+
+          {
+            path: '/misc/contribute',
+
+            file: 'misc-contribute'
+          }
+        ]
+      },
+
+      withExtAndWithLog: {
+        options: {
+          output: {
+            snapshotFileExt: 'html',
+
+            logFile: 'job-log.log',
+
+            dir: './tmp'
+          }
+        }
+      },
+
+      withoutExtAndWithoutLog: {
+        options: {
+          output: {
+            snapshotFileExt: false,
+
+            logFile: false,
+
+            dir: './tmp'
+          }
+        }
+      }
+    },
+
+    nodeunit: {
+      options: {
+        reporter: 'junit',
+        reporterOptions: {
+          output: 'test/report'
+        }
+      },
+
+      withFileExtAndLog: {
+        src: ['test/specs/test-for-files-with-ext-and-log-specs.js'],
+
+        options: {
+          reporter: 'junit',
+          reporterOptions: {
+            output: 'test/report'
+          }
+        }
+      },
+
+      withoutFileExtAndWithoutLog: {
+        src: ['test/specs/test-for-files-without-ext-and-without-log-specs.js']
+      }
+    }
+  });
+
+  grunt.loadTasks('tasks');
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  grunt.registerTask('testFilesWithExtensionAndWithLog', [
+    'clean',
+
+    'ng_html_snapshot:withExtAndWithLog',
+
+    'nodeunit:withFileExtAndLog'
+  ]);
+
+  grunt.registerTask('testFilesWithoutExtensionAndWithoutLog', [
+    'clean',
+
+    'ng_html_snapshot:withoutExtAndWithoutLog',
+
+    'nodeunit:withoutFileExtAndWithoutLog'
+  ]);
+
+  grunt.registerTask('test', [
+    'testFilesWithoutExtensionAndWithoutLog',
+
+    'testFilesWithExtensionAndWithLog'
+  ]);
+
+  grunt.registerTask('default', ['test']);
+};
---
+++ tasks/ng_html_snapshot.js
@@ -0,0 +1,37 @@
+/*
+ * grunt-ng-html-snapshot
+ * https://github.com/JasonRammoray/grunt-ng-html-snapshot
+ *
+ * Copyright (c) 2016 JasonRammoray
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.task.registerMultiTask('ng_html_snapshot', function() {
+    var phantom = require('phantomjs');
+
+    var childProcess = require('child_process');
+
+    var options = this.options();
+
+    // Default parameters
+
+    options.pageLoadingTimeout = options.pageLoadingTimeout || 100;
+
+    options.pageDocType = options.pageDocType || '<!doctype html>';
+
+    options.jobPath = options.jobPath || './node_modules/grunt-ng-html-snapshot/utils/phantom-job.js';
+
+    childProcess.execFileSync(
+      phantom.path,
+
+      [options.jobPath, JSON.stringify(options)],
+
+      {
+        timeout: 300000
+      }
+    );
+  });
+};
